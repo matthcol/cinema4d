381294106b315a8bf07b4decb5fc9b14058b9576
matthcol <matthias.colin@gmail.com>
2023-12-14 14:56:21 +0100

    add getter/setter/constructeur Person

--- src/main/java/org/example/cinema/dto/Person.java
+++ src/main/java/org/example/cinema/dto/Person.java
@@ -1,6 +1,40 @@
-public class Person {
+package org.example.cinema.dto;
 
-    public String nom;
+public class Person {
+    public Person(String name, String prenom, int age) {
+        this.name = name;
+        this.prenom = prenom;
+        this.age = age;
+    }
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String name;
     public String prenom;
     public int age;
 }
