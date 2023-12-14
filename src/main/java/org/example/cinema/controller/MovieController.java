5d76a401b9134a82d0ceeb663fc6d506e57d79f7
dbeucher <damien@example.com>
2023-12-14 13:44:54 +0100

    add swagger setup

--- src/main/java/org/example/cinema/controller/MovieController.java
+++ src/main/java/org/example/cinema/controller/MovieController.java
@@ -1,5 +1,9 @@
 package org.example.cinema.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.cinema.dto.Movie;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +16,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/movie")
+@Tag(name = "Movie API")
 public class MovieController {
 
+    @Operation(summary = "List all recent movies", description = "Returns a collection of recent movies")
+    @ApiResponses(value={
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+            @ApiResponse(responseCode = "404", description = "Not found - The product was not found")
+    })
     @GetMapping
     public List<Movie> recentMovies() {
         return List.of(
@@ -22,14 +32,15 @@ public class MovieController {
         );
     }
 
+    @Operation(summary = "Add a movie", description = "Returns the newly saved movie")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successfully created"),
+            @ApiResponse(responseCode = "400", description = "Bad Request")
+    })
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public Movie add(Movie movie) {
-<<<<<<< HEAD
         movie.setId((int) Math.round(Math.random()*100000));
-=======
-        movie.setId((int) Math.round(Math.random() * 1000));
->>>>>>> 24e057f (generate random id)
         return movie;
     }
 }
