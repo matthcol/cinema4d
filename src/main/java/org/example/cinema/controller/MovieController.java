c572e5680308177be5439a50be3547c7ba9cdda7
matthcol <matthias.colin@gmail.com>
2023-12-14 10:47:18 +0100

    add dto Movie

--- src/main/java/org/example/cinema/controller/MovieController.java
+++ src/main/java/org/example/cinema/controller/MovieController.java
@@ -1,15 +1,21 @@
 package org.example.cinema.controller;
 
+import org.example.cinema.dto.Movie;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/movie")
 public class MovieController {
 
-    @GetMapping("")
-    public String info(){
-        return "api movie";
+    @GetMapping
+    public List<Movie> recentMovies(){
+        return List.of(
+            Movie.builder().title("Oppenheimer").build(),
+                Movie.builder().title("Hunger Games").build()
+        );
     }
 }
---
+++ src/main/java/org/example/cinema/dto/Movie.java
@@ -0,0 +1,12 @@
+package org.example.cinema.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Movie {
+    private Integer id;
+    private String title;
+    private int year;
+}
