f5d56cd337afb72bca397611cd578e0c13eccab1
dbeucher <damien@example.com>
2023-12-14 13:44:54 +0100

    add swagger setup

--- src/main/java/org/example/cinema/dto/Movie.java
+++ src/main/java/org/example/cinema/dto/Movie.java
@@ -3,9 +3,11 @@ package org.example.cinema.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
 @Builder
-public class Movie {
+public class Movie implements Serializable {
     private Integer id;
     private String title;
     private int year;
