e78d56dacb1cef073e6b8f9920da3d8710f05986
matthcol <matthias.colin@gmail.com>
2023-12-14 10:29:00 +0100

    fix movie controller

--- src/main/java/org/example/cinema/controller/MovieController.java
+++ src/main/java/org/example/cinema/controller/MovieController.java
@@ -2,7 +2,9 @@ package org.example.cinema.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping("/api/movie")
 public class MovieController {
 
