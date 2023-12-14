7640be0b020dda2fd33f18cb8c29dec601457570
dbeucher <damien@example.com>
2023-12-14 11:42:16 +0100

    post new movie

--- src/main/java/org/example/cinema/controller/MovieController.java
+++ src/main/java/org/example/cinema/controller/MovieController.java
@@ -1,9 +1,8 @@
 package org.example.cinema.controller;
 
 import org.example.cinema.dto.Movie;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -12,10 +11,18 @@ import java.util.List;
 public class MovieController {
 
     @GetMapping
-    public List<Movie> recentMovies(){
+    public List<Movie> recentMovies() {
         return List.of(
-            Movie.builder().title("Oppenheimer").build(),
+                Movie.builder().title("Oppenheimer").build(),
                 Movie.builder().title("Hunger Games").build()
         );
     }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public Movie add(Movie movie) {
+        movie.setId(1);
+        return movie;
+    }
+
 }
