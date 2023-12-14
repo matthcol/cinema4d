0db0865f6107c86b069f77e050923cfece86fdc7
matthcol <matthias.colin@gmail.com>
2023-12-14 10:13:32 +0100

    first commit

---
+++ src/test/java/org/example/cinema/CinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
