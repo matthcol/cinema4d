dfa53f7d657fba6e264f399bb519fe0bdfc89787
dbeucher <damien@example.com>
2023-12-14 13:56:05 +0100

    setting up swagger annotations

--- src/main/java/org/example/cinema/controller/MovieController.java
+++ src/main/java/org/example/cinema/controller/MovieController.java
@@ -6,11 +6,16 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.cinema.dto.Movie;
 import org.springframework.http.HttpStatus;
+<<<<<<< HEAD
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+=======
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+>>>>>>> 211c689 (setting up swagger annotations)
 
 import java.util.List;
 
@@ -20,16 +25,16 @@ import java.util.List;
 public class MovieController {
 
     @Operation(summary = "List all recent movies", description = "Returns a collection of recent movies")
-    @ApiResponses(value={
+    @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
             @ApiResponse(responseCode = "404", description = "Not found - The product was not found")
     })
     @GetMapping
-    public List<Movie> recentMovies() {
-        return List.of(
+    public ResponseEntity<List<Movie>> recentMovies() {
+        return ResponseEntity.ok(List.of(
                 Movie.builder().title("Oppenheimer").build(),
                 Movie.builder().title("Hunger Games").build()
-        );
+        ));
     }
 
     @Operation(summary = "Add a movie", description = "Returns the newly saved movie")
