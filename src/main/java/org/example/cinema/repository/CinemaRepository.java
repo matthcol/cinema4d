bc60fed6dafbe7efe041c6c026e6062a77a2ebf4
matthcol <matthias.colin@gmail.com>
2023-12-15 09:32:03 +0100

    fix interface CinemaRepository

--- src/main/java/org/example/cinema/repository/CinemaRepository.java
+++ src/main/java/org/example/cinema/repository/CinemaRepository.java
@@ -2,12 +2,11 @@ package org.example.cinema.repository;
 
 
 import org.example.cinema.dto.Movie;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.repository.CrudRepository;
 import java.util.List;
-@SpringBootApplication
+
 public interface CinemaRepository extends CrudRepository<Movie, Long>{
-	Movie findById(long id);
+	
 	List <Movie> findByTitle(String title);
 
 }
